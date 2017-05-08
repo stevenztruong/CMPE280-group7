13bb1b1d4d48f1622752ad2c492a155aa734a9ff
hoangluong <hoang.luong@sjsu.edu>
2017-05-08 15:04:50 -0700

    add getByFirstName and getByLastName services (#1)
    * cleanup
    
    * add getByFirstName and getBylastname, add new schema for put
    
    * add getByFirstName and getByLastName URI

--- Controllers/developer.js
+++ Controllers/developer.js
@@ -1,5 +1,4 @@
 // import library
-const bodyParser = require('body-parser');
 const lodash = require('lodash');
 
 var developers = [
@@ -11,15 +10,11 @@ var developers = [
 ];
 
 module.exports.getDevelopers = function(req, res) {
-  if (req.query.firstName) {
-    getDevelopersByFName(req, res);
-    return;
-  }
   res.json(developers);
 };
 
 module.exports.getDevelopersByID = function (req, res) {
-  const developer = lodash.find(developers, ['id', req.params.id]);
+  const developer = lodash.find(developers, ['id', req.swagger.params.id.value]);
   if(!developer) {
     res.statusCode = 404;
     return res.send('Error 404: No developer found');
@@ -27,29 +22,25 @@ module.exports.getDevelopersByID = function (req, res) {
   res.json(developer);
 };
 
-const getDevelopersByFName = function (req,res) {
-  const developer = lodash.find(developers, ['firstName', req.query.firstName]);
-  if (!developer) {
+module.exports.getDevelopersByFName = function (req,res) {
+  const developer = lodash.filter(developers, ['firstName', req.swagger.params.firstName.value]);
+  if (!developer.length) {
     res.statusCode = 404;
     return res.send('Error 404: No developer found');
   }
   res.json(developer);
 }
 
-// module.exports.getDevelopersByLName = function (req,res) {
-//   const developer = lodash.find(developers, ['lastName', req.params.lastName]);
-//   if (!developer) {
-//     res.statusCode = 404;
-//     return res.send('Error 404: No developer found');
-//   }
-//   res.json(developer);
-// }
+module.exports.getDevelopersByLName = function (req,res) {
+  const developer = lodash.filter(developers, ['lastName', req.swagger.params.lastName.value]);
+  if (!developer.length) {
+    res.statusCode = 404;
+    return res.send('Error 404: No developer found');
+  }
+  res.json(developer);
+}
 
 module.exports.addDeveloper = function (req, res) {
-  if(!req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('firstName') || !req.body.hasOwnProperty('lastName')) {
-    res.statusCode = 400;
-    return res.send('Error 400: Post syntax incorrect.');
-  }
   const developer = lodash.find(developers, ['id', req.body.id]);
   if (developer) {
     res.statusCode = 409;
@@ -66,7 +57,7 @@ module.exports.addDeveloper = function (req, res) {
 }
 
 module.exports.deleteDeveloper = function (req, res) {
-  const developerIdx = lodash.findIndex(developers, ['id', req.params.id]);
+  const developerIdx = lodash.findIndex(developers, ['id', req.swagger.params.id.value]);
   if(developerIdx < 0) {
     res.statusCode = 404;
     return res.send('Error 404: No developer found');
@@ -77,19 +68,10 @@ module.exports.deleteDeveloper = function (req, res) {
 }
 
 module.exports.updateDeveloperByID = function (req ,res) {
-  const developer = lodash.find(developers, ['id', req.params.id]);
+  const developer = lodash.find(developers, ['id', req.swagger.params.id.value]);
   if(!developer) {
     res.statusCode = 404;
     return res.send('Error 404: No developer found');
   }
-  // Only set first name and last name if they are not empty
-  if (req.body.firstName) {
-    developer.firstName = req.body.firstName;
-  }
-
-  if (req.body.lastName) {
-    developer.lastName = req.body.lastName;
-  }
-
   res.json(developer);
 }
--- README.md
+++ README.md
@@ -6,7 +6,9 @@
 4. API URI: http://localhost:4000/
 5. Swagger UI: http://localhost:4000/docs
 6. GET all developers endpoint: http://localhost:4000/developers
-7. GET endpoint: http://localhost:4000/developers/:id
-8. POST endpoint: http://localhost:4000/developers/
-9. DELETE endpoint: http://localhost:4000/developers/:id
-10. PUT endpoint: http://localhost:4000/developers/:id
+7. GET developer by ID endpoint: http://localhost:4000/developers/:id
+8. GET developers by firstName endpoint: http://localhost:4000/developers/findByFirstName/:firstName
+9. GET developers by lastName endpoint: http://localhost:4000/developers/findByLastName/:lastName
+10. POST endpoint: http://localhost:4000/developers/
+11. DELETE endpoint: http://localhost:4000/developers/:id
+12. PUT endpoint: http://localhost:4000/developers/:id
